a827be2a16062b734a5169784c7eeb2450032f10
galbarcessat <galbarcessat1@gmail.com>
2024-04-02 21:12:33 +0300

    Header design + functionallity , favicon and title change

--- src/cmps/Header.jsx
+++ src/cmps/Header.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import AbraLogo from '../assets/imgs/AbraLogo.png'
 
 export function Header() {
-  return (
-    <div>Header</div>
-  )
+    const [currPage, setCurrPage] = useState('home')
+    const navigate = useNavigate()
+
+    function changePage(page) {
+        if (page === 'home') {
+            navigate('/')
+        } else {
+            navigate(`/${page}`)
+        }
+        setCurrPage(page)
+    }
+
+    function checkIsActive(page) {
+        return page === currPage
+    }
+
+    return (
+        <header className="header-container">
+            <img
+                onClick={() => navigate('/')}
+                src={AbraLogo}
+                alt="Abra" />
+            <div className="nav-links-container">
+                <span className={checkIsActive('home') ? 'active' : ''} onClick={() => changePage('home')}>Home</span>
+                <span className={checkIsActive('favorite') ? 'active' : ''} onClick={() => changePage('favorite')}>Favorites</span>
+            </div>
+        </header>
+    )
 }
